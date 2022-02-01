035e9eb85963c818adfca5564db4f06610875167
gavril-s <37928028+gavril-s@users.noreply.github.com>
2022-02-01 21:33:23 +0300

    405

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});*/
+    if (req.method === 'OPTIONS') 
+        res.send(200);
+    else 
+        next();
+});
 
 app.get('/info.json', (req, res) => {
     let info = {};
